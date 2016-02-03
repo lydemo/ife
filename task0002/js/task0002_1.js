d3f396739cc1e2934a5e217aa04f98de269e0acd
ly199106 <xiaohaitun1991@126.com>
2016-02-03 16:16:29 +0800

    task0002

--- task0002/js/task0002_1.js
+++ task0002/js/task0002_1.js
@@ -6,16 +6,16 @@ $.click("button", function() {
     console.log($("input"));
     console.log(content);
     content = trim(content); //去掉首位的空格
-    var contentArr = content.split(/,|，/);//由中文或者英文逗号隔开
+    var contentArr = content.split(/,+|，+/);//由中文或者英文逗号隔开
     var contentArr2 = deleteBlank(uniqArray(contentArr)); // 去重，去空白
 
-    var showDiv = $(".show");
+    var showDiv = $(".display");
    // var warnDiv = $(".warn");
 
  /*   if (contentArr2.length > 10 || contentArr2.length === 0) {
         warnDiv.style.display = "block"; */
 		if(contentArr2.length===0)
-        showDiv.style.display = "none";
+        displayDiv.style.display = "none";
  /*   } else {
         warnDiv.style.display = "none"; */
     //    var checkboxStr = "";
@@ -24,8 +24,8 @@ $.click("button", function() {
             checkboxStr += '<br><input type="checkbox"><label>' + contentArr2[i] + '</label>';
         }  */
      //   console.log(checkboxStr.substr(4));
-        showDiv.innerHTML =contentArr2 ;
-        showDiv.style.display = "block";
+        displayDiv.innerHTML =contentArr2 ;
+        displayDiv.style.display = "block";
 //    }
 
     console.log(contentArr2);
