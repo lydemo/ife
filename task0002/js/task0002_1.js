db7f2d541830ea00c47a79562b57f848793f6455
ly199106 <xiaohaitun1991@126.com>
2016-02-15 14:11:45 +0800

    task0002

--- task0002/js/task0002_1.js
+++ task0002/js/task0002_1.js
@@ -7,29 +7,29 @@ $.click("button", function() {
     console.log(content);
     content = trim(content); //去掉首位的空格
     var contentArr = content.split(/\n|\s+|,|，|;|；|、/);//允许用户用换行、空格（全角/半角）、逗号（全角/半角）、顿号、分号来作为不同爱好的分隔
-    var contentArr2 = deleteBlank(uniqArray(contentArr)); // 去重，去空白
+    var contentArr2 = deleteBlank(uniqArray(contentArr)); 
 
     var displayDiv = $(".hobby");
-   // var warnDiv = $(".warn");
+    var warnDiv = $(".warn");
 
- /*   if (contentArr2.length > 10 || contentArr2.length === 0) {
+   if (contentArr2.length > 10 || contentArr2.length === 0) {
         warnDiv.style.display = "block"; */
 		if(contentArr2.length===0)
         displayDiv.style.display = "none";
- /*   } else {
+   } else {
         warnDiv.style.display = "none"; */
-    //    var checkboxStr = "";
-    /*    for (var i = 0; i < contentArr2.length; i++) {
-            // contentArr2[i] = 0;
+       var checkboxStr = "";
+        for (var i = 0; i < contentArr2.length; i++) {
+             contentArr2[i] = 0;
             checkboxStr += '<br><input type="checkbox"><label>' + contentArr2[i] + '</label>';
         }  */
-     //   console.log(checkboxStr.substr(4));
+        console.log(checkboxStr.substr(4));
         displayDiv.innerHTML =contentArr2 ;
         displayDiv.style.display = "block";
-//    }
+    }
 
     console.log(contentArr2);
-    // $(".show").innerHTML = contentArr2;
+  //  $(".show").innerHTML = contentArr2;
 
 
 
